92c9f3bbf816061d2aebc5456eed59d2859b8825
Sean Ho <git@seanho.com>
2017-09-01 13:54:27 -0700

    PowerShell remoting for ansible

--- plays/win-setup.ps1
+++ plays/win-setup.ps1
@@ -1,5 +1,10 @@
-## win-setup.ps1
-# Windows PowerShell script
+#Requires -Version 3.0
+# win-setup.ps1
+# PowerShell script: 
+# On ansible control host: pip install "pywinrm>=0.2.2"
+
+$fw = 'netsh advfirewall firewall'
+$vpnonly = 'localip=10.100.1.0/24 remoteip=10.100.1.0/24'
 
 ###### tinc VPN
 
@@ -10,7 +15,6 @@ $tincEXE = 'tinc-install.exe'
 iwr -outfile "$tincEXE" "$tincURL"
 ".\$tincEXE" 
 cd 'C:\Program Files\tinc'
-tinc
 
 # Install TAP adapter
 cd tap-win64
@@ -20,7 +24,23 @@ cd ..
 # Configure the network interface and firewall
 netsh interface set interface name = "Ethernet 2" newname = "TincVPN"
 Set-NetConnectionProfile -InterfaceAlias TincVPN -NetworkCategory Private
-netsh advfirewall firewall add rule name=tinc dir=in action=allow localport=655 protocol=UDP
+$fw add rule name=tinc dir=in action=allow localport=655 protocol=UDP
+
+# Configure service
+tinc
+Start-Service -Name tinc
+
+###### Enable PowerShell Remoting
+# http://docs.ansible.com/ansible/latest/intro_windows.html
+# https://github.com/ansible/ansible/blob/devel/examples/scripts/ConfigureRemotingForAnsible.ps1
+Enable-PSRemoting -Force -ErrorAction Stop
+
+$psrule = 'rule name="Allow WinRM HTTPS"'
+$fwrule = $fw show $psrule
+If ($fwrule.count -ge 5) {
+  $fw del $psrule
+}
+$fw add $psrule dir=in action=allow localport=5986 protocol=TCP $vpnonly
 
 ###### Cygwin SSH
 
@@ -38,6 +58,5 @@ iwr "$cygEXE"
 
 # Configure ssh
 "$cyg\usr\bin\ssh-host-config" -p 26 -y
-netsh advfirewall firewall add rule name=ssh dir=in action=allow ^
-  localport=26 protocol=TCP remoteip=10.100.1.0/24
+$fw add rule name=ssh dir=in action=allow localport=26 protocol=TCP $vpnonly
 
